1ef234272342ec971a7560efca3f2ea1678aece8
linxingchen <linxingchen@baidu.com>
2017-07-08 08:09:24 +0800

    update

--- webpack/xy-travelnotes/src/App.vue
+++ webpack/xy-travelnotes/src/App.vue
@@ -1,23 +1,37 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view></router-view>
+    <bottom></bottom>
   </div>
 </template>
 
 <script>
+import Bottom from './pages/common/Bottom'
+
 export default {
-  name: 'app'
+    name: 'app',
+    data () {
+        return {
+
+        }
+    },
+    components: {
+        Bottom
+    }
 }
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  html,body,dl,ul,ol,menu,li,div,p,img,h1,h2,h3,h4,h5,h6 {
+    margin: 0;
+    padding: 0;
+    border: none;
+  }
+  html, body{ margin:0; height:100%; }
+  #app {
+    height: 100%
+  }
+  .bottom {
+  }
 </style>
--- webpack/xy-travelnotes/src/main.js
+++ webpack/xy-travelnotes/src/main.js
@@ -1,10 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueResource from 'vue-resource'
+import MuseUI from 'muse-ui'
+import 'muse-ui/dist/muse-ui.css'
+import 'muse-ui/dist/theme-light.css'
 import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
+
+Vue.use(MuseUI)
+Vue.use(VueResource)
 
 /* eslint-disable no-new */
 new Vue({
@@ -12,4 +19,9 @@ new Vue({
   router,
   template: '<App/>',
   components: { App }
+})
+
+Vue.http.interceptors.push((request, next) => {
+    request.credentials = true
+    next()
 })
